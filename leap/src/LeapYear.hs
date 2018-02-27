6effc11f1e00fe1e677536a464f27a0f8e82d435
Zack Hartmann <zackhartmann@gmail.com>
2018-02-27 18:17:10 -0500

    Implemented Alternative Logic

--- leap/src/LeapYear.hs
+++ leap/src/LeapYear.hs
@@ -2,4 +2,8 @@ module LeapYear (isLeapYear) where
 
 isLeapYear :: Integer -> Bool
 
-isLeapYear year = (mod year 4 == 0 && (mod year 100 /= 0 || mod year 400 == 0))
+isLeapYear year
+ | mod year 4 == 0 = True
+ | mod year 100 == 0 = False
+ | mod year 400 == 0 = True
+ | otherwise = False
